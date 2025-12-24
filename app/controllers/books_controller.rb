e6308282bd71900eb175033b91538fbb1db7c951
Ubuntu <245080863+mabosuke1133@>
2025-12-24 13:01:06 +0000

    新規投稿機能のViewとController(create)を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,12 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    ridirect_to '/books'
+  end
+
   def index
   end
 
@@ -11,4 +17,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+end
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :body)
 end
