ed40758a874c7847493120197a92662a762b3d3f
Ubuntu <245080863+mabosuke1133@>
2025-12-25 07:41:55 +0000

    フラッシュメッセージの実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,8 @@ class BooksController < ApplicationController
      @book = Book.new(book_params)
 
    if @book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
-
    else
     @books = Book.all
     render :index
@@ -33,6 +33,17 @@ class BooksController < ApplicationController
     book.destroy                  
     redirect_to '/books'          
   end
+
+  def update
+  @book = Book.find(params[:id])
+
+  if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+  else
+    render :edit
+  end
+end
 
 
   private
